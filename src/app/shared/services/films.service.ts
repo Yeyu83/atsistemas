e405bbeaa67caa6f00febf4207edb9e8eaf23cab
yeyu83 <chemabravoh@hotmail.com>
2022-04-03 12:52:37 +0200

    fix: interface film renamed to movie

--- src/app/shared/services/films.service.ts
+++ src/app/shared/services/films.service.ts
@@ -1,9 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Film } from '@models/interfaces/film.interface';
+import { Movie } from '@models/interfaces/movie.interface';
 import { environment } from 'environments/environment';
 import { map, Observable } from 'rxjs';
-
 
 @Injectable({
   providedIn: 'root',
@@ -15,12 +14,12 @@ export class FilmsService {
     private readonly http: HttpClient,
   ) { }
 
-  public getFilms(): Observable<Film[]> {
-    return this.http.get<Film[]>(this.MOVIES_API_URL);
+  public getFilms(): Observable<Movie[]> {
+    return this.http.get<Movie[]>(this.MOVIES_API_URL);
   }
 
-  public getFilmById(id: number): Observable<Film | undefined> {
-    return this.http.get<Film[]>(this.MOVIES_API_URL)
-      .pipe(map((movies: Film[]) => movies.find((movie: Film) => movie.id === Number(id))));
+  public getFilmById(id: number): Observable<Movie | undefined> {
+    return this.http.get<Movie[]>(this.MOVIES_API_URL)
+      .pipe(map((movies: Movie[]) => movies.find((movie: Movie) => movie.id === Number(id))));
   }
 }
