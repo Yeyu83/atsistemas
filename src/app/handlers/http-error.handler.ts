cdf40f050a5cc14605ea43844775dcdda3c82906
yeyu83 <chemabravoh@hotmail.com>
2022-04-03 14:15:45 +0200

    feat: notification service integration

--- src/app/handlers/http-error.handler.ts
+++ src/app/handlers/http-error.handler.ts
@@ -1,11 +1,16 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { ErrorHandler, Injectable } from '@angular/core';
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import { NotificationService } from '@shared/services/notification.service';
 
 @Injectable()
 export class HttpErrorHandler implements ErrorHandler {
+  constructor(
+    private injector: Injector,
+  ) { }
+
   handleError(error: Error | HttpErrorResponse) {
     if (error instanceof HttpErrorResponse) {
-      alert(error.message)
+      this.injector.get(NotificationService).showError(error.message);
     }
   }
 }
