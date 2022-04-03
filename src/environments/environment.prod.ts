8c53169cb48887f3b1a108d5aa92cb274cacc1d6
yeyu83 <chemabravoh@hotmail.com>
2022-04-03 11:29:03 +0200

    feat: api routes to environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  api: {
+    url: 'http://localhost:3000',
+    endpoints: {
+      movies: 'movies',
+      actors: 'actors',
+      companies: 'companies',
+    },
+  },
 };
