21e6f0b4f3870e0b5d3736a9c926151f629e4aee
yeyu83 <chemabravoh@hotmail.com>
2022-04-03 20:47:36 +0200

    feat: layout state integration

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { HeaderState } from '@models/interfaces/layout-state.interface';
+import { LayoutService } from '@app/shared/services/layout.service';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +10,16 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  public headerState$: Observable<HeaderState | undefined> | undefined
+
+  constructor(
+    private readonly layoutService: LayoutService,
+    public readonly location: Location,
+  ) { }
+
+  ngOnInit(): void {
+    this.headerState$ = this.layoutService.getLayoutState()
+      .pipe(map(layoutState => layoutState?.header))
+  }
 }
