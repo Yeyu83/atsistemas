d75d183e963efde6f0ddadc992467e73b8888df5
yeyu83 <chemabravoh@hotmail.com>
2022-04-03 12:12:22 +0200

    feat: ngx-translate added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,13 @@
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CoreRoutingModule } from './core-routing.module';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [],
@@ -9,6 +15,14 @@ import { CoreRoutingModule } from './core-routing.module';
     CommonModule,
     CoreRoutingModule,
     HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+      defaultLanguage: 'es',
+    }),
   ]
 })
 export class CoreModule { }
