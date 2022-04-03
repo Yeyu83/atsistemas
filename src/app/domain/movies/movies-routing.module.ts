eb0d9406bfa5a79eb172258c1a2c75cd24af635d
yeyu83 <chemabravoh@hotmail.com>
2022-04-03 16:27:48 +0200

    feat: movie-detail route

--- src/app/domain/movies/movies-routing.module.ts
+++ src/app/domain/movies/movies-routing.module.ts
@@ -4,18 +4,30 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'all',
+    component: MoviesComponent,
+  },
+  {
+    path: 'detail/:id',
+    loadChildren: () => import('@domain/movie-detail/movie-detail.module').then((m) => m.MovieDetailModule),
+  },
+];
+
+const redirectRoutes: Routes = [
+  {
     path: '',
     redirectTo: 'all',
     pathMatch: 'full',
   },
   {
-    path: 'all',
-    component: MoviesComponent,
+    path: 'detail',
+    redirectTo: 'all',
+    pathMatch: 'full',
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild([...redirectRoutes, ...routes])],
   exports: [RouterModule],
 })
 export class MoviesRoutingModule { }
