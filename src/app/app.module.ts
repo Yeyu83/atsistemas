075696b5eff25323395814979db2680adb78ec69
Yeyu83 <51865562+Yeyu83@users.noreply.github.com>
2022-04-03 11:38:10 +0200

    feat: core module added (#4)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
+import { CoreModule } from '@app/core/core.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,9 +10,8 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
